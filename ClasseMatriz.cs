943561978310f6c1ae41ff9bfebea680da107d03
Murilll <murilosertorio25@gmail.com>
2022-11-07 11:50:02 -0300

    ._.

---
+++ ClasseMatriz.cs
@@ -0,0 +1,15 @@
+public class Matriz
+{
+    public int Dimensao1 { get; set; }
+    public int Dimensao2 { get; set; }
+    public Matriz (int dimensao1, int dimensao2)
+    {
+        this.Dimensao1 = Dimensao1;
+        this.Dimensao2 = Dimensao2;
+
+        if (Dimensao1 == dimensao2)
+        {
+            
+        }
+    }
+}
